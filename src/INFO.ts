6b41a33cd769a0f4283720dbb93cdc380bac4198
the-chat <thechatllc@gmail.com>
2022-03-07 17:14:54 +0500

    LEGAL -> INFO

---
+++ src/INFO.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_INFO_HOST = "https://the-chat-info.vercel.app" as const
+export const legalUrl = "/legal" as const
+export const aboutUrl = "/about" as const
--- src/LEGAL.ts
+++
@@ -1,3 +0,0 @@
-export const DEFAULT_LEGAL_HOST = "https://the-chat-legal.vercel.app" as const
-export const infoUrl = "/legal" as const
-export const aboutUrl = "/about" as const
