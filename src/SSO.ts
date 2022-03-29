168930b94188b7e646dd56291973f71630def249
the-chat <thechatllc@gmail.com>
2022-03-29 17:20:27 +0500

    naming

--- src/SSO.ts
+++ src/SSO.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_SSO_HOST = "https://the-chat-sso.vercel.app" as const
+export const HOST = "https://the-chat-sso.vercel.app" as const
 export const getSSOTokenUrl = "/api/token" as const
 export const editUrl = "/edit" as const
 export const changeUrl = "/control" as const
